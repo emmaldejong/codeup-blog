3a67c8c39d7633cf144adbadfd15bfa89f99c957
emmadejong <dejong.l.emma@gmail.com>
2018-04-16 14:18:09 -0500

    init commit

---
+++ src/main/java/com/codeup/codeupblog/CodeupBlogApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.codeupblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeupBlogApplication.class, args);
+	}
+}
