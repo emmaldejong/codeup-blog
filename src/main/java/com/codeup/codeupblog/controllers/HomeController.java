d6dc24b7988e9915b43dbe270db4db7870cc0f6d
emmadejong <dejong.l.emma@gmail.com>
2018-04-17 10:04:50 -0500

    changed welcome controller to use home.html view

--- src/main/java/com/codeup/codeupblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupblog/controllers/HomeController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String welcome() {
+//        return "This is the landing page!";
+//    }
+
+    // using views instead of response body
     @GetMapping("/")
-    @ResponseBody
     public String welcome() {
-        return "This is the landing page!";
+        return "hello";
     }
 }
