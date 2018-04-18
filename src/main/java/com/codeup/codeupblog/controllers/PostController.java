c77276b1e7412abef7c185193009ef2d8516f004
emmadejong <dejong.l.emma@gmail.com>
2018-04-18 09:49:37 -0500

    added all solution code through views

--- src/main/java/com/codeup/codeupblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupblog/controllers/PostController.java
@@ -1,31 +1,50 @@
 package com.codeup.codeupblog.controllers;
 
+import com.example.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String index() {
-        return "posts index page";
+    public String index(Model model) {
+
+        List<Post> posts = new ArrayList<>();
+
+        posts.add(new Post("Post Title 1", "This is a description of post 1."));
+        posts.add(new Post("Post Title 2", "This is a description of post 2."));
+        posts.add(new Post("Post Title 3", "This is a description of post 3."));
+        posts.add(new Post("Post Title 4", "This is a description of post 4."));
+
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String show(@PathVariable long id) {
-        return "view an individual post";
+    public String show(@PathVariable long id, Model model) {
+        Post post = new Post("Test Post", "This is a test description.");
+        model.addAttribute("post", post);
+        return "/posts/show";
     }
+
     @GetMapping("/posts/create")
     @ResponseBody
     public String create() {
-        return "view the form for creating a post";
+        return "Here is the post create form...";
     }
+
     @PostMapping("/posts/create")
     @ResponseBody
     public String insert() {
-        return "create a new post";
+        return "Inserted new post!";
     }
+
 }
