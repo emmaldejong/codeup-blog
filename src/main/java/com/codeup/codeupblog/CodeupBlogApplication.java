f694ffe3720479c10484a7147e57cc32cb2dc883
emmadejong <dejong.l.emma@gmail.com>
2018-04-23 14:50:34 -0500

    added biuld config file

--- src/main/java/com/codeup/codeupblog/CodeupBlogApplication.java
+++ src/main/java/com/codeup/codeupblog/CodeupBlogApplication.java
@@ -2,11 +2,17 @@ package com.codeup.codeupblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeupBlogApplication {
+public class CodeupBlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeupBlogApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CodeupBlogApplication.class);
 	}
 }
